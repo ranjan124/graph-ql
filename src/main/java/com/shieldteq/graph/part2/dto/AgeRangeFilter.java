49a6eaada7496758c2bec1286f291db76db9a33c
Ranjan <ranjanjena124@gmail.com>
2025-04-21 01:09:24 +0200

    GraphQl Lessons - Part 2

---
+++ src/main/java/com/shieldteq/graph/part2/dto/AgeRangeFilter.java
@@ -0,0 +1,4 @@
+package com.shieldteq.graph.part2.dto;
+
+public record AgeRangeFilter(Integer min, Integer max) {
+}
---
+++ src/main/java/com/shieldteq/graph/part2/dto/Customer.java
@@ -0,0 +1,9 @@
+package com.shieldteq.graph.part2.dto;
+
+public record Customer(
+        int id,
+        String name,
+        Integer age,
+        String city
+) {
+}
